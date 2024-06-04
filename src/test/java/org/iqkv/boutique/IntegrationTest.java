8117999653af688cde31bec99bea826d0ab40388
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-06-04 20:11:40 +0300

    Initial version of Boutique generated by generator-jhipster@8.5.0
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/test/java/org/iqkv/boutique/IntegrationTest.java
@@ -0,0 +1,21 @@
+package org.iqkv.boutique;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.iqkv.boutique.config.AsyncSyncConfiguration;
+import org.iqkv.boutique.config.EmbeddedSQL;
+import org.iqkv.boutique.config.JacksonConfiguration;
+import org.iqkv.boutique.config.TestSecurityConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BoutiqueApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
